5955080d1da52f1d7727b7eff5df31303aa74d78
Bernhard Frauendienst <freenas@nospam.obeliks.de>
2019-02-04 22:10:23 +0100

    Update freenas disk code for FreeNAS 11.2

--- spinpid/sensors/disk/freenas.py
+++ spinpid/sensors/disk/freenas.py
@@ -2,16 +2,13 @@ from .. import Temperature
 from . import Disk, DiskSource, DiskError, MeanDiskTemperatureSource 
 from .smart import parse_smart_temperature
 from middlewared.client.client import Client
+from middlewared.common.camcontrol import camcontrol_list
+from middlewared.common.smart.smartctl import get_smartctl_args
 from middlewared.utils import Popen, run
 import subprocess
 
 c = Client()
 
-try:
-    from middlewared.common.smart.smartctl import get_smartctl_args
-except ImportError:
-    async def get_smartctl_args(devname):
-        return c.call('disk._DiskService__get_smartctl_args', devname)
 
 class FreenasDiskSource(DiskSource):
     def get_all_disk_names(self):
@@ -19,11 +16,16 @@ class FreenasDiskSource(DiskSource):
         return (d['name'] for d in c.call('disk.query', [('togglesmart', '=', True)]))
 
     async def get_all_disks(self):
-        return (FreenasDisk(d) for d in self.get_all_disk_names())
+        devices = await camcontrol_list()
+        return (FreenasDisk(devname, devices) for devname in self.get_all_disk_names() if devname in devices)
 
 class FreenasDisk(Disk):
+    def __init__(self, devname, devices):
+        super().__init__(devname)
+        self.devices = devices
+
     async def get_temperature(self) -> Temperature:
-        args = await get_smartctl_args(self.device)
+        args = await get_smartctl_args(c, self.devices, self.device)
         if not args:
             raise DiskError(f"Unable to determine smartctl args for disk {self.device}")
         p1 = await Popen(['smartctl', '--attributes', '--nocheck=standby'] + args, stdout=subprocess.PIPE)
