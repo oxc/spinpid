5aa551565349a5ec0e9b381980fee5dbbc0cec72
Bernhard Frauendienst <git@nospam.obeliks.de>
2025-09-04 16:41:26 +0200

    fix: upgrade cpu sensor for TrueNAS Scale 25.04

--- spinpid/interfaces/truenas/cpu.py
+++ spinpid/interfaces/truenas/cpu.py
@@ -7,7 +7,8 @@ from spinpid.interfaces.truenas.middleware import TrueNASClient
 class TrueNASCPUTemperaturesSource(TemperaturesSource, TrueNASClient):
     async def get_all_temperatures(self) -> Iterator[Temperature]:
         temps: dict[str, float] = self.middleware.call('reporting.cpu_temperatures')
+        del temps["cpu"]
         return (
             Temperature(temp, f"CPU {idx}")
-                for idx, temp in sorted(temps.items(), key=lambda x: int(x[0]))
+                for idx, temp in sorted(temps.items(), key=lambda x: int(x[0].removeprefix("cpu")))
         )
